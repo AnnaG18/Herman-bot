526e10970bbe9906416579e3680e43fd8eefc548
Gorr <annagorr@web.de>
2018-04-24 09:33:55 +0200

    logging of messages to conversations

--- middleware/logMessages.js
+++ middleware/logMessages.js
@@ -23,7 +23,7 @@ function createConversation(id, data) {
             id,
             messages: [],
             user: data.user,
-            source: data.source
+            source: data.source,
         });
 
         conversations[id] = conversation;
@@ -36,8 +36,8 @@ function logMessage(source, event) {
     const from = source === 'user' ? event.address.user.name : 'bot' ;
     const to = source === 'bot' ? event.address.user.name : 'bot';
     const conversation = conversations[conversationId] || createConversation(conversationId, {
-        user: event.address.user.name,
-        source: event.source
+        user: event.address.user,
+        source: event.source,
     });
     conversation.messages.push(getMessageObjectFromEvent(from, to, event));
 
@@ -52,10 +52,32 @@ function getMessageObjectFromEvent(from, to, event) {
     };
 }
 
-function saveConversation(id) {
-    id in conversations && conversations[id].save().then((result) => {
+async function saveConversation(id) {
+    const localConversation = conversations[id];
+    try{
+        const conversation = await Conversation.findOne({id});
+        if(conversation !== null) {
+            //add messages from local conversation to db conversation
+            conversation.messages = conversation.messages.concat(localConversation.messages);
+            conversation.save();
+
+        }else{
+            localConversation.save();
+        }
+        //deletion of local conversation so we do not log messages twice.
+        // Immediately delete (don't wait for save succes),
+        // so we won't lose messages arriving in the meantime.
         delete conversations[id];
-    });
+    }
+    catch(err){
+        //  if an error occurs while saving (network gone, etc.), re-add the local conversation
+        // so we have another got at saving at a later time.
+        // Only do this if the conversation was deleted before.
+        if(!id in conversations){
+           conversations[id] = localConversation;
+        }
+            console.warn(` Error saving conversation with id $(id).`);
+    }
 }
 
 const logger = (source) => (event, next) => {
